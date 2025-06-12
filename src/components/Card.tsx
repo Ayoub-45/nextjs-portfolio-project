ee5a7268ac78f7ef419f74360fc6b066bfe79048
Ayoub-45 <ayoubnasraoui161@yahoo.fr>
2025-06-12 13:32:15 +0100

    fix build issue

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,15 +1,29 @@
-"use client"
+"use client";
+
 import React from 'react';
-interface myCardProps{
-    project: any;
+
+interface Project {
+  title: string;
+  desc: string;
 }
-const Card = ({project}:myCardProps) => {
-    return (
-<div   className="bg-gray-100 dark:bg-gray-800 shadow-md p-6 rounded-xl hover:shadow-xl transition">
-  <h3 className="text-xl font-semibold text-gray-900 dark:text-white">{project.title}</h3>
-  <p className="mt-2 text-gray-700 dark:text-gray-300">{project.desc}</p>
-</div>
-    );
+
+interface CardProps {
+  project: Project;
 }
+
+const Card: React.FC<CardProps> = ({ project }) => {
+  return (
+    <article
+      className="bg-gray-100 dark:bg-gray-800 shadow-md hover:shadow-lg hover:-translate-y-1 transform transition-all duration-300 ease-in-out p-6 rounded-xl"
+    >
+      <h3 className="text-xl font-bold text-gray-900 dark:text-white">
+        {project.title}
+      </h3>
+      <p className="mt-2 text-gray-700 dark:text-gray-300">
+        {project.desc}
+      </p>
+    </article>
+  );
+};
 
 export default Card;
